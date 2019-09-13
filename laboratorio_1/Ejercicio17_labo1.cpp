01b2ed5455612098a505802040b06d35b40f0671
miranda00 <diegomiranda0799@gmail.com>
2019-09-13 12:49:07 -0600

    correccion

--- laboratorio_1/Ejercicio17_labo1.cpp
+++ laboratorio_1/Ejercicio17_labo1.cpp
@@ -4,29 +4,28 @@ using namespace std;
 
 
 int main(void) {
-    
+
     float promeio;
     int notas[10];
     int suma=0;
-    
-    
+
+
     for (int i=0; i <10; i++){
     cout<< "Ingrese nota "<< i+1 << endl;
-    cin>>notas[i]; 
-   
+    cin>>notas[i];
+
     }
     int n = 9;
     int i =0;
+    int promedio;
     while ( i<=n){
-   
+
     suma=suma + notas[i];
     i++;
     }
-    
-     cout<< suma;    
-   
+    promedio=(suma/n);
+    cout<<"El promedio es de: "<<promedio<<endl;
 
-    
     return 0;
 }
 
